42e8b74e7f7aaedfdde39e9ae7b8ee21bd46b327
Dima <uzhdimka@gmail.com>
2024-12-31 01:52:34 +0100

    ig sleep is really necessary

--- test/backend/nixos_web.sh
+++ test/backend/nixos_web.sh
@@ -1,24 +1,10 @@
-GO_BINARY=$(which go)
-if [ -z "$GO_BINARY" ]; then
-  echo "Error: Go binary not found. Please ensure Go is installed and available in your PATH."
-  exit 1
-fi
-
-GOROOT=$(dirname $(dirname "$GO_BINARY"))
-if [ ! -d "$GOROOT" ]; then
-  echo "Error: GOROOT directory $GOROOT does not exist."
-  exit 1
-fi
-
-CACHE_DIR="/home/$USER/.cache/go-build"
-
 env -i \
-  GOARCH="amd64" \
-  GOPATH="$HOME/.local/go" \
-  GOROOT="$GOROOT/share/go" \
-  GOCACHE="$CACHE_DIR" \
+  GOARCH="$GOARCH" \
+  GOPATH="$GOPATH" \
+  GOROOT="$GOROOT" \
+  GOCACHE="$GOCACHE" \
   USER="$USER" \
-  XDG_CACHE_HOME="$HOME/.cache" \
-  HOME="/home/$USER" \
-  PATH="$HOME/.local/go/bin:$PATH" \
+  XDG_CACHE_HOME="$XDG_CACHE_HOME" \
+  HOME="$HOME" \
+  PATH="$GOPATH/bin:$PATH" \
   "$GOPATH/bin/wasmserve" ./test/backend
