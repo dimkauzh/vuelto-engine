86abf0aa92b0ca904f8ef8ca61a58a8dcdeafc5f
Zaka <zakahacecosas@protonmail.com>
2025-01-01 16:20:13 +0100

    update license comment

--- internal/window/win_web.go
+++ internal/window/win_web.go
@@ -6,7 +6,7 @@
  *
  * This file is part of the Vuelto project, licensed under the VL-Cv1.1 License.
  * Primary License: GNU GPLv3 or later (see <https://www.gnu.org/licenses/>).
- * If unmaintained, this software defaults to the MIT License as per Vuelto License V1,
+ * If unmaintained, this software defaults to the MIT License as per Vuelto License V1.1,
  * at which point the copyright no longer applies.
  *
  * Distributed WITHOUT ANY WARRANTY; without even the implied warranty of
